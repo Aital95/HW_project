4d9704bb2fc176fe877dc2cef54bd026637728a8
user <aytem95@gmail.com>
2023-04-12 01:34:16 +0600

    add_upper_functions

--- str_func.py
+++ str_func.py
@@ -1,3 +1,15 @@
 def to_uppercase(s):
     """Принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     return s.upper()
+
+
+def convert_to_uppercase(string):
+    """Преобразование заданной строки в верхний регистр"""
+    return string.upper()
+
+
+def capitalize_first_letters(string):
+    """Заглавные первые буквы каждого слова в строке"""
+    words = string.split()
+    capitalized_words = [word.capitalized() for word in words]
+    return ' '.join(capitalized_words)
